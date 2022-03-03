0f6652ef696e79424f714029043ba1330751481c
palash barman <palash900202@gmail.com>
2022-03-04 01:50:46 +0600

    tic tac toe

---
+++ lib/ui/color.dart
@@ -0,0 +1,9 @@
+
+// now Let's add the colo9r that we will need to project
+import 'package:flutter/material.dart';
+
+class MainColor{
+  static Color primaryColor = Color(0xFF00061a);
+  static Color secondaryColor = Color(0xFF001456);
+  static Color accentColor = Color(0xFF4169e8);
+}
